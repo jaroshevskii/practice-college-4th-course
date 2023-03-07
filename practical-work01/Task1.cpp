cc6d760bf52eed0eae418bfc662016545f2e88e9
Sasha Jaroshevskii <72662383+jaroshevskii@users.noreply.github.com>
2023-03-07 07:15:15 -0800

    Create Task1.cpp

---
+++ practical-work01/Task1.cpp
@@ -0,0 +1,36 @@
+//
+// Практична Робота 1: Завдання 1
+//
+// Програму запитує у користувача число і виводить на екран перші числа
+// Фібоначчі. Програма реалізована з використанням циклу `while`.
+//
+
+#include <iostream>
+
+int main() {
+  std::cout << "Введіть число: ";
+  int number = 0;
+  std::cin >> number;
+
+  if (number < 0) {
+    std::cout << "Помилка: Число не може бути відємним.";
+  }
+  
+  int previous1 = 0, previous2 = 1, next = 0;
+  
+  int index = 0;
+  while (index < number) {
+    if (index <= 1) {
+      next = index;
+    } else {
+      next = previous1 + previous2;
+      previous1 = previous2;
+      previous2 = next;
+    }
+    std::cout << next << " ";
+    ++index;
+  }
+
+  std::cout << '\n';
+  return 0;
+}
