11517c2923e36bba02315511f2e050d60399179b
Sasha Jaroshevskii <jaroshevskiisasha@gmail.com>
2023-03-11 09:11:27 -0800

    Add task 1

---
+++ PracticalWork04/Task1/task1.py
@@ -0,0 +1,28 @@
+#
+# Практика (4 курс коледжу)
+# Практична робота 4: Завдання 1
+#
+
+import os
+
+try:
+    # Відкриття файлів для читання та запису
+    with open("Input.txt", "r") as file_input, open("Count.txt", "w") as file_count:
+
+        # Читання всіх символів з файлу Input.txt
+        text = file_input.read()
+
+        # Знаходження кількості символів
+        count = len(text)
+
+        # Запис кількості символів в файл Count.txt
+        file_count.write(str(count))
+
+        # Отримання повного шляху до файлу з результатами та виведення його у консоль
+        full_file_count_path = os.path.abspath(file_count.name)
+        print(f"""
+Results saved in '{full_file_count_path}'.
+        """)
+
+except Exception as error:
+    print(f"Error: {error}")
